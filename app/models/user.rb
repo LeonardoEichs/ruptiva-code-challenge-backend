b40bf30612d02ccb14a12092c9eb703ce837d9db
LeonardoEichs <leonardoeichs@gmail.com>
2020-03-25 01:17:52 -0300

    User Models Devise set up and enum fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :trackable, :rememberable, :timeoutable
   include DeviseTokenAuth::Concerns::User
+  enum role: { user: 0, admin: 1 }
 end
