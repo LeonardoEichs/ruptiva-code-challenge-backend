3d5efcbe45e090a8e39f691251277856af520644
LeonardoEichs <leonardoeichs@gmail.com>
2020-03-26 21:05:29 -0300

    Added rspec to the project and set up the testing database

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
          :trackable, :rememberable, :timeoutable
   include DeviseTokenAuth::Concerns::User
   enum role: { user: 0, admin: 1 }
+
+  def as_json
+    {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      role: role
+    }
+  end
 end
