f73801f02dac5bf8910ce5c6fa8dddb6846209ab
LeonardoEichs <leonardoeichs@gmail.com>
2020-03-29 01:34:56 -0300

    Correcting validations in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :trackable, :rememberable, :timeoutable
   include DeviseTokenAuth::Concerns::User
   enum role: { user: 0, admin: 1 }
-
-
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
 end
