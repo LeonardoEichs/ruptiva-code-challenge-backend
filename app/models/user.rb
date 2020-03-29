0d627cfcfe3b7cc8bac0b01ae0d2bf5626b2d79b
LeonardoEichs <leonardoeichs@gmail.com>
2020-03-29 02:13:59 -0300

    Correct user model encrypted_password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true
-  validates :password, :presence => true
+  validates :encrypted_password, :presence => true
 end
